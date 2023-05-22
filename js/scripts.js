b583df248d68d3af7508a0570727ab77e50402bb
Adolfo20bm <abm2491@gmail.com>
2023-05-22 10:08:03 -0500

    querySelector

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// querySelector // Puedes seleccionar el id con #
+const heading = document.querySelector('.header__texto h2'); // 0 o 1 Elementos
+heading.textContent = 'MODIFICADOOOOOOO';
+console.log(heading);
+
+//
