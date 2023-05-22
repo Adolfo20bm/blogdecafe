b1d8d5fac5c09b7cb497f2a9ad1b40280ca31240
Adolfo20bm <abm2491@gmail.com>
2023-05-22 10:19:31 -0500

    querySelectorAll

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ const heading = document.querySelector('.header__texto h2'); // 0 o 1 Elementos
 heading.textContent = 'MODIFICADOOOOOOO';
 console.log(heading);
 
-//
+// querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
+
+enlaces[0].textContent = 'MODIFICADODODO';
+enlaces[0].href = 'http://google.com';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion_enlace');
+
+//getElementById
+
+const heading2 = document.getElementById('heading');
+console.log(heading2);
